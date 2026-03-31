2c0b8332561187e2b97908ee85bdc5604dbc7a42
Your Name <you@example.com>
2026-03-31 17:54:44 +0530

    fix: remove node_modules and .env from tracking, add gitignore

---
+++ backend/server/controllers/authController.js
@@ -0,0 +1,5 @@
+const registerUser = async (req, res) => {};
+const verifyOTP = async (req, res) => {};
+const loginUser = async (req, res) => {};
+
+module.exports = { registerUser, verifyOTP, loginUser };
